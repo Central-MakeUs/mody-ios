25da6735018346769363c85c2ebdc38cd072887a
Jumy <kdjun97@gmail.com>
2026-07-14 20:09:56 +0900

    feat: overlay 추가

--- Projects/Feature/Main/Sources/MainContainer/Extensions/MainContainerViewController+Overlay.swift
+++ Projects/Feature/Main/Sources/MainContainer/Extensions/MainContainerViewController+Overlay.swift
@@ -9,14 +9,54 @@ import UIKit
 import SnapKit
 
 extension MainContainerViewController {
-    func showOverlay(_ overlayView: UIView) {
-        currentOverlayView?.removeFromSuperview()
+    func showOverlay(_ overlayViewController: UIViewController) {
+        removeCurrentOverlay()
 
-        view.addSubview(overlayView)
-        overlayView.snp.makeConstraints {
+        addChild(overlayViewController)
+        overlayViewController.view.alpha = 0
+        overlayViewController.view.backgroundColor = .clear
+        view.addSubview(overlayViewController.view)
+        overlayViewController.view.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        overlayViewController.didMove(toParent: self)
+        currentOverlayViewController = overlayViewController
 
-        currentOverlayView = overlayView
+        UIView.animate(withDuration: 0.2) {
+            overlayViewController.view.alpha = 1
+        }
+    }
+
+    func dismissOverlay(completion: (() -> Void)? = nil) {
+        guard let overlayViewController = currentOverlayViewController else {
+            completion?()
+            return
+        }
+
+        currentOverlayViewController = nil
+        overlayViewController.willMove(toParent: nil)
+
+        UIView.animate(
+            withDuration: 0.2,
+            animations: {
+                overlayViewController.view.alpha = 0
+            },
+            completion: { _ in
+                overlayViewController.view.removeFromSuperview()
+                overlayViewController.removeFromParent()
+                completion?()
+            }
+        )
+    }
+}
+
+private extension MainContainerViewController {
+    func removeCurrentOverlay() {
+        guard let overlayViewController = currentOverlayViewController else { return }
+
+        overlayViewController.willMove(toParent: nil)
+        overlayViewController.view.removeFromSuperview()
+        overlayViewController.removeFromParent()
+        currentOverlayViewController = nil
     }
 }
