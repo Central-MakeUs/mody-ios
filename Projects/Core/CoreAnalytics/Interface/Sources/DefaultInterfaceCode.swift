a4bc871407093474053601aef7437be6f94a321f
Jumy <kdjun97@gmail.com>
2026-08-17 13:37:09 +0900

    feat: AmplitudeService 기능 개발

--- Projects/Core/CoreAnalytics/Interface/Sources/DefaultInterfaceCode.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DefaultInterfaceCode.swift
-//  CoreAnalyticsInterface
-//
-//  Created by 김동준 on 8/17/26.
-//
-
-public protocol CoreAnalyticsInterface {}
---
+++ Projects/Core/CoreAnalytics/Interface/Sources/UseCase/AnalyticsUseCaseProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AnalyticsUseCaseProtocol.swift
+//  CoreAnalyticsInterface
+//
+//  Created by 김동준 on 8/17/26.
+//
+
+public protocol AnalyticsUseCaseProtocol {
+    func setUserID(_ userID: String?)
+    func log(_ event: AmplitudeLogEvent)
+    func viewDidLoad(screenName: String)
+}
